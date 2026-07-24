edd6299dfb67690ff80510214f7120f91fe4a49b
Aizenbens <sheninozen@gmail.com>
2026-07-24 16:41:49 -0700

    Update Scene.jsx

--- js/game/Scene.jsx
+++ js/game/Scene.jsx
@@ -1,17 +1,53 @@
 import { Canvas } from "@react-three/fiber";
 
+function Ground() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+      <planeGeometry args={[100, 100]} />
+      <meshStandardMaterial color="#4CAF50" />
+    </mesh>
+  );
+}
+
+function Cube() {
+  return (
+    <mesh position={[0, 0.5, 0]} castShadow>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function Lights() {
+  return (
+    <>
+      <ambientLight intensity={1.2} />
+      <directionalLight
+        position={[10, 15, 10]}
+        intensity={2}
+        castShadow
+      />
+    </>
+  );
+}
+
 export default function Scene() {
   return (
     <Canvas
-      camera={{ position: [0, 2, 5], fov: 75 }}
-      style={{ width: "100vw", height: "100vh" }}
+      shadows
+      camera={{
+        position: [0, 2, 6],
+        fov: 75
+      }}
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: "#87CEEB"
+      }}
     >
-      <ambientLight intensity={2} />
-
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <Lights />
+      <Ground />
+      <Cube />
     </Canvas>
   );
 }
